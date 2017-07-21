6156d75531951abb113a3bc6cca35f2e160839f9
Allen Lin <allen830423@gmail.com>
2017-07-21 10:01:51 +0800

    remove config and add sample

---
+++ client/src/config/config.sample.js
@@ -0,0 +1,7 @@
+module.exports.firebaseConfig = {
+  apiKey: 'apiKey',
+  authDomain: 'authDomain.firebaseapp.com',
+  databaseURL: 'https://databaseURL.firebaseio.com',
+  storageBucket: 'storageBucket.appspot.com',
+  messagingSenderId: 'messagingSenderId',
+};
